5c2a7eef1c68f01f792adfbfaf1adab042e1a934
PixelSergey <sergey.ichtchenko@gmail.com>
2020-09-14 19:09:30 +0300

    Finalise PlayerLook script

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class PlayerLook : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
-    
+    public float mouseX;
+    public float mouseY;
+    public Transform cameraTransform;
+
+    [Header("Miten paljon pelaaja saa katsoa ylös/alas?")]
+    public float maxX = 50f;
+    public float minX = -50f;
+
+    public float xRotation = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;  // Lukitsee hiiren
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation = xRotation - mouseY;
+        if(xRotation > maxX) xRotation = maxX;
+        if(xRotation < minX) xRotation = minX;
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        cameraTransform.Rotate(Vector3.up * mouseX);
     }
 }
