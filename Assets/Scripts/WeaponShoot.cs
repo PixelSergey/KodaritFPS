74e9e1cc650a277c7dc0c653b38418d93b0674c0
PixelSergey <sergey.ichtchenko@gmail.com>
2020-10-19 19:51:47 +0300

    Add shooting mechanic

---
+++ Assets/Scripts/WeaponShoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponShoot : MonoBehaviour
+{
+    public float shootForce = 10f;
+    public GameObject bulletPrefab;
+    Camera playerCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody>().AddForce(playerCam.transform.forward * shootForce, ForceMode.Impulse);
+        }
+    }
+}
