06d0f0ba5975939325f0542e14bc75cdf8f7dd3a
PixelSergey <sergey.ichtchenko@gmail.com>
2020-11-02 18:31:41 +0200

    Add better shooty mechanics

--- Assets/Scripts/WeaponShoot.cs
+++ Assets/Scripts/WeaponShoot.cs
@@ -6,7 +6,11 @@ public class WeaponShoot : MonoBehaviour
 {
     public float shootForce = 10f;
     public GameObject bulletPrefab;
+    public GameObject bullet = null;
+    public float holdStart;
+    public float maxHoldTime = 2f;
     Camera playerCam;
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +22,20 @@ public class WeaponShoot : MonoBehaviour
     void Update()
     {
         if(Input.GetButtonDown("Fire1")){
-            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-            bullet.GetComponent<Rigidbody>().AddForce(playerCam.transform.forward * shootForce, ForceMode.Impulse);
+            holdStart = Time.time;
+        }
+
+        if(Input.GetButtonUp("Fire1")){
+            if(bullet != null){
+                Destroy(bullet);
+            }
+
+            float holdTime = Time.time - holdStart;
+            float forceMultiplier = holdTime / maxHoldTime;
+            if(forceMultiplier > 1f) forceMultiplier = 1f;
+
+            bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody>().AddForce(playerCam.transform.forward * shootForce * forceMultiplier, ForceMode.Impulse);
         }
     }
 }
