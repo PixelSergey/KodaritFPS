5018cb2d48915b5d7c7659602b66672bc6092613
PixelSergey <sergey.ichtchenko@gmail.com>
2020-11-23 19:25:52 +0200

    Add terrain colours and title scene

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider who){
+        if(who.gameObject.tag == "Player"){
+            LoadScene();
+        }
+    }
+
+    public void LoadScene(){
+        SceneManager.LoadScene(0);
+    }
+}
